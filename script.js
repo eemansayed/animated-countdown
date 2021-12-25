6942f1329a4d3c521c110dbb9f24be678e11cd2c
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-25 09:25:24 +0000

    add function called resetDom

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ const replay = document.querySelector("#replay");
 
 runAnimation();
 
+function resetDom() {
+  counter.classList.remove("hide");
+  finalMessage.classList.remove("show");
+
+  nums.forEach((num) => {
+    num.classList.value = "";
+  });
+
+  nums[0].classList.add("in");
+}
+
 function runAnimation() {
   nums.forEach((num, idx) => {
     const nextToLast = nums.length - 1;
